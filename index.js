6d93cc30f3ec7531d29de504137ec0a46ac2a4e6
rainverted <MY_NAME@example.com>
2021-08-03 19:09:41 +0300

    final code

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ Jei pirkejas duoda per didele pinigu suma, kasininkas sako: "Aciu, jog pirkote! 
 const kasininkas = require('./kasininkas');
 
 (async () => {
+
+
+    const a = await kasininkas(100, [20, 50]);
+    console.log(a);
+
+    const b = await kasininkas(100, [20, 50, 20, 10, 5, 5]);
+    console.log(b);
+
+    const c = await kasininkas(100, [50, 20, 10, 5, 5, 1]);
+    console.log(c);
+
+
+
     // Reikia patiems iskviesti funkcija ir jai duoti tokias reiksmes:
     // a) kaina: 100; kupiuros: [20, 50]
     // b) kaina: 100; kupiuros: [20, 50, 20, 10, 5, 5]
--- kasininkas.js
+++ kasininkas.js
@@ -1,5 +1,25 @@
 async function kasininkas(price, cash) {
+    console.log(`Priimtos kupiuros: ${cash.join(', ')}.`);
 
+    let total = 0;
+    for (const number of cash) {
+        total += number;
+    }
+
+    let answer = '';
+    if (total < price) {
+        answer = 'Nepakanka pinigu!\n';
+    } else if (total === price) {
+        answer = 'Aciu, jog pirkote!\n';
+    } else {
+        answer = `Aciu, jog pirkote! Stai jusu graza - ${total - price} pinigu!\n`;
+    }
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(answer);
+        }, 2000);
+    })
 }
 
 module.exports = kasininkas;
